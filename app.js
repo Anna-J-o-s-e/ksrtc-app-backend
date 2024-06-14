4e73a5703fe1e7b73464b083cb11c6d8ee71a2f5
Anna-J-o-s-e <annajose941@gmail.com>
2024-06-14 09:26:51 +0530

    bug fixed

--- app.js
+++ app.js
@@ -1,116 +1,118 @@
-const mongoose=require("mongoose")
-const express=require("express")
-const cors=require("cors")
-const {ksrtcsmodel}=require("./models/ksrtc")
-const jwt=require("jsonwebtoken")
+const mongoose = require("mongoose")
+const express = require("express")
+const cors = require("cors")
+const { ksrtcsmodel } = require("./models/ksrtc")
+const jwt = require("jsonwebtoken")
 
-const bcrypt=require("bcryptjs") //encryption
+const bcrypt = require("bcryptjs") //encryption
 const { busesmodel } = require("./models/bus")
 
-const app=express()
+const app = express()
 app.use(cors())
 app.use(express.json())
 
-const generateHashedPassword=async (password)=>{
-    const salt=await bcrypt.genSalt(10)
-    return bcrypt.hash(password,salt)
+const generateHashedPassword = async (password) => {
+    const salt = await bcrypt.genSalt(10)
+    return bcrypt.hash(password, salt)
 }
 
 mongoose.connect("mongodb+srv://annajose:annajose01@cluster0.d4hgr.mongodb.net/ksrtcdb?retryWrites=true&w=majority&appName=Cluster0")
 
 
 //signup api
-app.post("/signup",async (req,res)=>{
-   let input=req.body
-   let hashedPassword=await generateHashedPassword(input.password)
-   console.log(hashedPassword)
-   input.password=hashedPassword
-   let ksrtc=new ksrtcsmodel(input)
-   ksrtc.save()
-   res.json({"status":"success"})
+app.post("/signup", async (req, res) => {
+    let input = req.body
+    let hashedPassword = await generateHashedPassword(input.password)
+    console.log(hashedPassword)
+    input.password = hashedPassword
+    let ksrtc = new ksrtcsmodel(input)
+    ksrtc.save()
+    res.json({ "status": "success" })
 })
 
 //signin api
-app.post("/signin",(req,res)=>{
-    let input=req.body
-    ksrtcsmodel.find({"email":req.body.email}).then((response)=>{
-        if (response.length>0) {
-          let dbPassword=response[0].password
-          console.log(dbPassword)
-          bcrypt.compare(input.password,dbPassword,(error,isMatch)=>{
-            if (isMatch) {
-                jwt.sign({email:input.email},"ksrtc-app",{expiresIn:"1d"},(error,token)=>{
-                    if (error) {
-                        res.json({"status":"Unable To create Token"})
-                    } else {
-                       res.json({"status":"success","userId":response[0]._id,"token":token}) 
-                    }
-                })
-                
-            } else {
-               res.json({"status":"Incorrect Password"}) 
-            }
-          })  
+app.post("/signin", (req, res) => {
+    let input = req.body
+    ksrtcsmodel.find({ "email": req.body.email }).then((response) => {
+        if (response.length > 0) {
+            let dbPassword = response[0].password
+            console.log(dbPassword)
+            bcrypt.compare(input.password, dbPassword, (error, isMatch) => {
+                if (isMatch) {
+                    jwt.sign({ email: input.email }, "ksrtc-app", { expiresIn: "1d" }, (error, token) => {
+                        if (error) {
+                            res.json({ "status": "Unable To create Token" })
+                        } else {
+                            res.json({ "status": "success", "userId": response[0]._id, "token": token })
+                        }
+                    })
+
+                } else {
+                    res.json({ "status": "Incorrect Password" })
+                }
+            })
         } else {
-         res.json({"status":"User Not Found"})   
+            res.json({ "status": "User Not Found" })
         }
     }).catch()
 })
 
 
-app.post("/viewusers",(req,res)=>{
-    let token=req.headers["token"]
-    jwt.verify(token,"ksrtc-app",(error,decoded)=>{
+app.post("/viewusers", (req, res) => {
+    let token = req.headers["token"]
+    jwt.verify(token, "ksrtc-app", (error, decoded) => {
         if (error) {
-            res.json({"status":"Unauthorized access"})
+            res.json({ "status": "Unauthorized access" })
         } else {
             if (decoded) {
-             ksrtcsmodel.find().then(
-                (response)=>{
-                    res.json(response)
-                }
-             ).catch()   
+                ksrtcsmodel.find().then(
+                    (response) => {
+                        res.json(response)
+                    }
+                ).catch()
             }
         }
     })
 })
 
-app.post("/add",(req,res)=>{
-    let input=req.body
-    let bus=new busesmodel(input)
+app.post("/add", (req, res) => {
+    let input = req.body
+    let bus = new busesmodel(input)
     bus.save()
     console.log(bus)
-    res.json({"status":"success"})
+    res.json({ "status": "success" })
 })
 
-app.post("/view",(req,res)=>{
+app.post("/view", (req, res) => {
     busesmodel.find().then(
-        (data)=>{
+        (data) => {
             res.json(data)
         }
-    ).catch((error)=>{
+    ).catch((error) => {
         res.json(error)
     })
 })
 
-app.post("/search",(req,res)=>{
-    let input=req.body
-    busesmodel.find(input).then((data)=>{
+app.post("/search", (req, res) => {
+    let input = req.body
+    busesmodel.find(input).then((data) => {
         res.json(data)
-    }).catch((error)=>{
+    }).catch((error) => {
         res.json(error)
     })
 })
 
-app.post("/delete",(req,res)=>{
-    let input=req.body
-    busesmodel.findByIdAndDelete(input._id).then((response)=>{
-        res.json({"status":"success"})
-    }).catch((error)=>{
-        res.json({"status":"error"})
+app.post("/delete", (req, res) => {
+    let input = req.body
+    busesmodel.findByIdAndDelete(input._id).then(
+        (response) => {
+            console.log("delete")
+        res.json({ "status": "success" })
+    }).catch((error) => {
+        res.json({ "status": "error" })
     })
 })
 
-app.listen(8080,()=>{
+app.listen(8080, () => {
     console.log("server started")
 })
